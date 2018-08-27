688c521f565a10ab0c0afbacaeab28efa80892e5
mtgo007 <36502935+mtgo007@users.noreply.github.com>
2018-08-27 08:38:39 -0300

    correção conflitos e modulos

--- src/Js/index.js
+++
@@ -1,8 +0,0 @@
-import iziToast from "../../node_modules/izitoast";
-
-function alertSuccess(msg) {
-    iziToast.show({
-        title: "Sucesso!",
-        message: msg
-    });
-}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './Css/index.css';
 import './Css/login.css';
 import './Css/signUp.css';
-import './Js/index.js';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
